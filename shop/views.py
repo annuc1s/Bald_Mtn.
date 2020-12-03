42abb5ce7b2c202ddaa55f766379a1f705f10a18
annuc1s <annuciis121@gmail.com>
2020-12-03 17:28:43 +0000

    added products views

--- shop/views.py
+++ shop/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from .models import Product
 
-
-def index(request):
+def all_products(request):
     """ A view to go to main shop page """
 
-    return render(request, 'shop/shop.html')
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'shop/shop.html', context)
