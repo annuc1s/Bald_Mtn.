4e12420b3874d366f8910607ad643a2d24e3699d
annuc1s <annuciis121@gmail.com>
2020-12-03 17:14:59 +0000

    customized admin

--- shop/models.py
+++ shop/models.py
@@ -3,6 +3,10 @@ from django.db import models
 
 # Category model for sorting by name
 class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
     name = models.CharField(max_length=150)
     friendly_name = models.CharField(max_length=150, null=True, blank=True)
 
