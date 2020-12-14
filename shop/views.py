8012b43859cfab9240ae8ba6a578e802c1694fdd
annuc1s <annuciis121@gmail.com>
2020-12-14 17:39:30 +0000

    added search funtionality

--- shop/views.py
+++ shop/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Product
 
 
@@ -6,9 +8,23 @@ def all_products(request):
     """ A view to go to main shop page """
 
     products = Product.objects.all()
+    query = None
+
+    """ Search query will return results if matched with
+    name and/or description by using Django db model Q """
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "Search field cannot be empty!")
+                return redirect(reverse('shop'))
+
+        queries = Q(name__icontains=query) | Q(description__icontains=query)
+        products = products.filter(queries)
 
     context = {
         'products': products,
+        'search_term': query,
     }
 
     return render(request, 'shop/shop.html', context)
