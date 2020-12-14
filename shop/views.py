a724e7e77522993a9f708fc83ddc28d0037b8610
annuc1s <annuciis121@gmail.com>
2020-12-14 15:42:20 +0000

    added a product_detail template and view

--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
+
 
 def all_products(request):
     """ A view to go to main shop page """
@@ -11,3 +12,15 @@ def all_products(request):
     }
 
     return render(request, 'shop/shop.html', context)
+
+
+def product_detail(request, product_id):
+    """ A view to go to a specific product details page """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'shop/product_detail.html', context)
