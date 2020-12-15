e2ccbf23ca29660cdc9363d086ef48f3dd1e41b9
annuc1s <annuciis121@gmail.com>
2020-12-15 15:08:34 +0000

    added products to home page, temporarily for content

--- home/views.py
+++ home/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from shop.models import Product
+
 
 def index(request):
     """ A view to return the index page """
 
-    return render(request, 'home/index.html')
+    products = Product.objects.order_by('sku').all()[:8]
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'home/index.html', context)
+
