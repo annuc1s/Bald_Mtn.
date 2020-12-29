49175dfbb1c0efe6bf49137920042d393cc6c140
annuc1s <annuciis121@gmail.com>
2020-12-29 15:52:23 +0000

    sort functinality added to products

--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
+from django.db.models.functions import Lower
 from .models import Product
 
 
@@ -12,6 +13,19 @@ def all_products(request):
     sort = None
     direction = None
 
+    if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+
     """ Search query will return results if matched with
     name and/or description by using Django db model Q """
     if request.GET:
@@ -21,8 +35,8 @@ def all_products(request):
                 messages.error(request, "Search field cannot be empty!")
                 return redirect(reverse('shop'))
 
-        queries = Q(name__icontains=query) | Q(description__icontains=query)
-        products = products.filter(queries)
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
 
     current_sorting = f'{sort}_{direction}'
 
