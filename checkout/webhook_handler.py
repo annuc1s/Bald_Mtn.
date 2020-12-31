ff2e4db0b13977dbfd7a2dfd1653cad854c20483
annuc1s <annuciis121@gmail.com>
2020-12-31 17:13:15 +0000

    webhook payment intent added

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -12,18 +12,18 @@ class StripeWH_Handler:
         Handle a generic/unknown/unexpected webhook event
         """
         return HttpResponse(
-            content=f'Webhook received: {event["type"]}',
+            content=f'Unhandled webhook received: {event["type"]}',
             status=200)
-
 
     def handle_payment_intent_succeeded(self, event):
         """
         Handle the payment_intent.succeeded webhook from Stripe
         """
+        intent = event.data.object
+        print(intent)
         return HttpResponse(
             content=f'Webhook received: {event["type"]}',
             status=200)
-
 
     def handle_payment_intent_payment_failed(self, event):
         """
