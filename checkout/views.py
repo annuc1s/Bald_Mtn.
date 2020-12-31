bc0af4fcf7d7bcc11578948dfdeb840bbfc60369
annuc1s <annuciis121@gmail.com>
2020-12-31 15:59:37 +0000

    checkout_success url added

--- checkout/views.py
+++ checkout/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.conf import settings
 
 from .forms import OrderForm
-from .models import OrderLineItem
+from .models import Order, OrderLineItem
 from shop.models import Product
 from bag.context import bag_contents
 
