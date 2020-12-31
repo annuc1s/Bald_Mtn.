ea08fdd4958ca1eb73b05c2ec85cba635e1875fc
annuc1s <annuciis121@gmail.com>
2020-12-31 20:42:43 +0000

    deployment part 2

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn bald_mtn.wsgi:application
--- bald_mtn/settings.py
+++ bald_mtn/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '69r_a#4wd=%4bpm(37eo*71i@&co7jgj4+3)gd*g@a73je3@6i'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['baldmtn.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -121,12 +121,17 @@ WSGI_APPLICATION = 'bald_mtn.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
